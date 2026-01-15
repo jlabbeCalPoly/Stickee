64e17c94ea33853042392cf003921802ad7899bb
Julian Labbe <jlabbe@calpoly.edu>
2026-01-14 21:59:15 -0800

    Initial Commit

---
+++ Stickee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stickee
+//
+//  Created by Julian Labbe on 1/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stickee/StickeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickeeApp.swift
+//  Stickee
+//
+//  Created by Julian Labbe on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct StickeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
